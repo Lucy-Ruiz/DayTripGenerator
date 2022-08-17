f74015db173ecd2681406ab799be2a3a3a0dd01c
Lucy Ruiz <lucyruiz846@gmail.com>
2022-08-17 11:15:42 -0700

    Updated functions from four into one generic. Created function for final day trip

--- daytripgenerator.py
+++ daytripgenerator.py
@@ -5,61 +5,39 @@ restaurant_options = ['local food restaurant', 'high end restaurant', 'convinien
 transportation_options = ['train', 'bus', 'bicycle', 'subway', 'car rental', 'taxi']
 entertaiment_options = ['museums', 'theater', 'markets', 'concerts', 'picnic']
 
-def destinations(destination_options):
+def get_random_selection(list_of_options, string_output, result_text):
     confirmation = 'n'
     while confirmation == 'n':
-        selected_destination = random.choice(destination_options)
-        confirmation = input(f'Would you like to visit {selected_destination}? y/n: ')
-    return selected_destination
+        selected_choice = random.choice(list_of_options)
+        confirmation = input(f'{string_output} {selected_choice}? y/n ')
+    print(f'{result_text} {selected_choice}')
+    return selected_choice
 
-def restaurants(restaurant_options):
+
+def final_day_trip(destination, restaurant, transportation, entertaiment):
     confirmation = 'n'
     while confirmation == 'n':
-        selected_restaurant = random.choice(restaurant_options)
-        confirmation = input(f'Would you like to eat at {selected_restaurant}? y/n: ')
-    return selected_restaurant
+        confirmation = input('Would you like to complete your day trip? y/n: ')
+        if confirmation != 'n':
+            break
+        ask_for_update = input('Type which option would you like to change? destination/restaurant/transportation/entertaiment: ')
+        if ask_for_update == 'destination':
+            destination = get_random_selection(destination_options, 'Would you like the destination of', 'Your destination is')
+        elif ask_for_update == 'restaurant':
+            restaurant = get_random_selection(restaurant_options, 'Would you like to eat at', 'Your foodie match is')
+        elif ask_for_update == 'transportation':
+            transportation = get_random_selection(transportation_options, 'Would you like to move through the city in', 'Your transportation mode is')
+        elif ask_for_update == 'entertaiment':
+            entertaiment = get_random_selection(entertaiment_options, 'Would you like to go to', 'Enjoy your time in the')
+    result = f'Your next trip will be in {destination}, you can enjoy the city via {transportation} and going to {entertaiment}. When feeling hungry you can go to a {restaurant}.'
+    print(result)
 
-def transportation(transportation_options):
-    confirmation = 'n'
-    while confirmation == 'n':
-        selected_transportation = random.choice(transportation_options)
-        confirmation = input(f'Would you like to move through the city via {selected_transportation}? y/n: ')
-    return selected_transportation
+my_destination = get_random_selection(destination_options, 'Would you like the destination of', 'Your destination is')
 
-def entertaiment(entertaiment_options):
-    confirmation = 'n'
-    while confirmation == 'n':
-        selected_entertaiment = random.choice(entertaiment_options)
-        confirmation = input(f'Would you like to go to {selected_entertaiment}? y/n: ')
-    return selected_entertaiment
+my_restaurant = get_random_selection(restaurant_options, 'Would you like to eat at', 'Your foodie match is')
 
+my_transportation = get_random_selection(transportation_options, 'Would you like to move through the city in', 'Your transportation mode is')
 
-my_destination = destinations(destination_options)
-print(f'Your destination is {my_destination}')
+my_entertaiment = get_random_selection(entertaiment_options, 'Would you like to go to', 'Enjoy your time in the')
 
-my_restaurant = restaurants(restaurant_options)
-print(f'You may enjoy eating at a {my_restaurant}')
-
-my_transportation = transportation(transportation_options)
-print(f'Transportation mode in the city is {my_transportation}')
-
-my_entertaiment = entertaiment(entertaiment_options)
-print(f'Enjoy your time {my_entertaiment}')
-
-
-confirmation = 'n'
-while confirmation == 'n':
-    confirmation = input('Would you like to complete your day trip? y/n: ')
-    if confirmation != 'n':
-        break
-    ask_for_update = input('Type which option would you like to change? destination/restaurant/transportation/entertaiment: ')
-    if ask_for_update == 'destination':
-        my_destination = destinations(destination_options)
-    elif ask_for_update == 'restaurant':
-        my_restaurant = restaurants(restaurant_options)
-    elif ask_for_update == 'transportation':
-        my_transportation = transportation(transportation_options)
-    elif ask_for_update == 'entertaiment':
-        my_entertaiment = entertaiment(entertaiment_options)
-
-print(f'Your next trip will be in {my_destination}, you can enjoy the city via {my_transportation} and going to {my_entertaiment}. When feeling hungry you can go to a {my_restaurant}')
+final_day_trip(my_destination, my_restaurant, my_transportation, my_entertaiment)
